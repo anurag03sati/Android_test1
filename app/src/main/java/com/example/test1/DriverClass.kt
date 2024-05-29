af970c4d40f5a0e337c438c6edcce67de2f3f31b
Anurag <anurag03sati@gmail.com>
2024-05-29 11:58:48 +0530

    initial commit

---
+++ app/src/main/java/com/example/test1/DriverClass.kt
@@ -0,0 +1,10 @@
+package com.example.test1
+
+object DriverClass{
+    @JvmStatic
+    fun main(args: Array<String>){
+        val abc=1
+        println(abc)
+        println("hello world")
+    }
+}
