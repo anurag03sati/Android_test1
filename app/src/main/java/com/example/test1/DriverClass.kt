8a6349f5d4c66e6ee5b872ed1709c7ab3f8b17b3
Anurag <anurag03sati@gmail.com>
2024-05-29 14:23:44 +0530

    static in java

--- app/src/main/java/com/example/test1/DriverClass.kt
+++ app/src/main/java/com/example/test1/DriverClass.kt
@@ -6,5 +6,10 @@ object DriverClass{
         val abc=1
         println(abc)
         println("hello world")
+
+        var emp1:Employee = Employee()
+        Employee.PI = 81.0
+        emp1.operation1()
+        emp1.operation2()
     }
 }
---
+++ app/src/main/java/com/example/test1/Employee.java
@@ -0,0 +1,14 @@
+package com.example.test1;
+
+public class Employee {
+    static double PI=3.14;
+    public void operation1(){
+        PI+=3.15;
+        System.out.println(PI);
+    }
+
+    public void operation2(){
+        PI+=3.16;
+        System.out.println(PI);
+    }
+}
